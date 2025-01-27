8dcc5ee8cb75cb7411ee5ebddc02ab87046a9d93
Aleksandr Kajl <kajlaleksandr1986@gmail.com>
2025-01-27 12:25:14 +0700

    Change output level GPIO LED

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -51,14 +51,13 @@ void MX_GPIO_Init(void)
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOB);
 
   /**/
-  LL_GPIO_ResetOutputPin(LED_GPIO_Port, LED_Pin);
+  LL_GPIO_SetOutputPin(LED_GPIO_Port, LED_Pin);
 
   /**/
   GPIO_InitStruct.Pin = LED_Pin;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
   LL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
 }
