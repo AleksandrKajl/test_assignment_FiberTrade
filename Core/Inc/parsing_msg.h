0958eefa6c46c2fa9f5f675b0157e4f2c8cfea31
Aleksandr Kajl <kajlaleksandr1986@gmail.com>
2025-01-28 15:42:17 +0700

    Add parsing msg for led on and off.

---
+++ Core/Inc/parsing_msg.h
@@ -0,0 +1,18 @@
+//
+// Created by Aleksandr on 27.01.2025.
+//
+
+#ifndef TEST_ASSIGNMENT_FIBERTRADE_PARSING_MSG_H
+#define TEST_ASSIGNMENT_FIBERTRADE_PARSING_MSG_H
+#include "ring_buf.h"
+
+typedef enum Receive_msg {
+    LED1_ON = 0,
+    LED2_ON,
+    LED1_OFF,
+    LED2_OFF
+}Receive_msg_t;
+
+void get_msg(RING_buffer_t *rx_buf, RING_buffer_t *cmd_buff);
+
+#endif //TEST_ASSIGNMENT_FIBERTRADE_PARSING_MSG_H
---
+++ Core/Src/parsing_msg.c
@@ -0,0 +1,42 @@
+//
+// Created by Aleksandr on 27.01.2025.
+//
+#include "parsing_msg.h"
+#include <string.h>
+
+const uint8_t gk_cmd_on[] = "led1 on";
+const uint8_t gk_cmd_off[] = "led1 off";
+
+#define MAXCMD_SZ  20
+uint8_t buf[MAXCMD_SZ];
+
+void get_msg(RING_buffer_t *rx_buf, RING_buffer_t *cmd_buf)
+{
+    const uint16_t k_buff_sz = RING_get_count(rx_buf);
+    if (!k_buff_sz) {
+        return;
+    }
+
+    uint16_t cmdend_idx = 0;
+    while (cmdend_idx < k_buff_sz) {
+        buf[cmdend_idx] = RING_pop(rx_buf);
+        if (buf[cmdend_idx] == '\r') {
+            buf[cmdend_idx++] = 0;
+            break;
+        }
+        cmdend_idx++;
+    }
+
+    if (!strcmp(buf, gk_cmd_on)) {
+        RING_put(LED1_ON, cmd_buf);
+    } else if (!strcmp(buf, gk_cmd_off)) {
+        RING_put(LED1_OFF, cmd_buf);
+    }
+
+    memset(buf, 0x00, MAXCMD_SZ);
+
+    if (cmdend_idx < k_buff_sz) {
+        get_msg(rx_buf, cmd_buf);
+    }
+}
+
